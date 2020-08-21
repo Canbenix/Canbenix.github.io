f4d35718721e59dfb45ba1cec6f98b5170265df6
Canbenix <cantbenix@gmail.com>
2020-08-21 02:37:22 +0200

    Updates

---
+++ python/demo.py
@@ -0,0 +1,9 @@
+num_cells = 3
+
+def sendMessage(text=None, braille=None):
+  return { "text": text, "braille": braille }
+
+def newMessage(message):
+  print(message)
+  global num_cells
+  return sendMessage(text="Hello, back!", braille="⠇⠳⠊⠎")
