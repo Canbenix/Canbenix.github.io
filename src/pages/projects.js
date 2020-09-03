09c5495539351b8bc6f71d1edd4f72294131a823
Canbenix <cantbenix@gmail.com>
2020-09-03 21:15:37 +0200

    refactor + add comments

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,6 +4,50 @@ import React from "react"
 import SEO from "../components/seo"
 
 const ProjectsFlexpulse = () => {
+
+  const projects_flexpulse = [
+    // twee-weekse roosters
+    // vertalingssysteem
+    // standaard tekst onder emails
+    // factuur splits standaard
+    // intake overzicht
+    // leerlingbron
+    // regios
+    // dagboeken
+    // firma-gegevens aanpassen
+    // herinnering keuring, sms saldo
+    // voortgangskaarten aanpasbaar
+    // customizable invoice
+    // /leerling contactformulier
+    // email afzender
+    // standaard notities
+    // mails per categorie
+    // mailen naar werknemers en bedrijven
+    // priomelding
+    // factuuradres, afhaaladres
+    // inplannen bij alle instr
+    // facturen achteraf bewerken
+    // franchises
+    // resellers
+    // live Google, iOS, Outlook calendar subscription to the user's calendar
+    // email statussen aflever-, verzend- en open-status
+    // dynamische agenda
+    // notitiecategorien
+    // ical with appt email
+    // overigetypes
+    // bedrijven aan leerlingen
+    // geocode koppeling
+    // emails x days before/after y
+    // concept facturen autom. versturen
+    // urenoverzicht aflettering
+    // cbr aan lestypes
+    {
+      name: "",
+      description: "[Coming soon]",
+      path: "M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253",
+    },
+  ]
+
   return (
     <div className="mt-4 relative max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 lg:mt-5">
       <div className="rounded-lg bg-blue-200 px-6 py-8 sm:p-10 lg:flex lg:items-center">
@@ -28,102 +72,35 @@ const ProjectsFlexpulse = () => {
             including pupil and employee management, accountancy, invoice tools and personal schedules and calendars.
             Some of the projects where I was the lead developer include:
             <ul className="mt-8 lg:grid lg:grid-cols-2 lg:col-gap-8 lg:row-gap-5">
-              <li
-                className="mt-5 first:mt-0 lg:mt-0 flex items-start lg:col-span-1"
-                key="todo1"
-              >
-                <div className="flex-shrink-0">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    stroke-width="2"
-                    viewBox="0 0 24 24"
-                    class="w-8 h-8"
-                  >
-                    <path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path>
-                  </svg>
-                </div>
-                <div className="ml-3 text-sm leading-5 text-gray-700">
-                  <p>
-                    [Coming soon]
-                  </p>
-                </div>
-              </li>
-              <li
-                className="mt-5 first:mt-0 lg:mt-0 flex items-start lg:col-span-1"
-                key="todo2"
-              >
-                <div className="flex-shrink-0">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    stroke-width="2"
-                    viewBox="0 0 24 24"
-                    class="w-8 h-8"
-                  >
-                    <path d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path>
-                  </svg>
-                </div>
-                <div className="ml-3 text-sm leading-5 text-gray-700">
-                  <p>
-                    [Coming soon]
-                  </p>
-                </div>
-              </li>
-              <li
-                className="mt-5 first:mt-0 lg:mt-0 flex items-start lg:col-span-1"
-                key="todo3"
-              >
-                <div className="flex-shrink-0">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    stroke-width="2"
-                    viewBox="0 0 24 24"
-                    class="w-8 h-8"
-                  >
-                    <path d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                  </svg>
-                </div>
-                <div className="ml-3 text-sm leading-5 text-gray-700">
-                  <p>
-                    [Coming soon]
-                  </p>
-                </div>
-              </li>
-              <li
-                className="mt-5 first:mt-0 lg:mt-0 flex items-start lg:col-span-1"
-                key="todo4"
-              >
-                <div className="flex-shrink-0">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    stroke-width="2"
-                    viewBox="0 0 24 24"
-                    class="w-8 h-8"
-                  >
-                    <path d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z"></path>
-                  </svg>
-                </div>
-                <div className="ml-3 text-sm leading-5 text-gray-700">
-                  <p>
-                    [Coming soon]
-                  </p>
-                </div>
-              </li>
+              {projects_flexpulse.reverse().map(({ name, description, path, i }) => (
+                <li
+                  className="mt-5 first:mt-0 lg:mt-0 flex items-start lg:col-span-1"
+                  key="project_flexpulse_{i}"
+                >
+                  <div className="flex-shrink-0">
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      viewBox="0 0 24 24"
+                      className="w-8 h-8"
+                    >
+                      <path d={path}></path>
+                    </svg>
+                  </div>
+                  <div className="ml-3 text-sm leading-5 text-gray-700">
+                    <p>
+                      {description}
+                    </p>
+                  </div>
+                </li>
+              ))}
             </ul>
           </div>
 
-          <div className="mt-10 text-base text-gray-900">
+          <div className="mt-10 text-base text-gray-900 text-center">
             Because I'm a sucker for statistics, here are some cool quantative numbers about my time at Flexpulse:
           </div>
 
@@ -134,6 +111,7 @@ const ProjectsFlexpulse = () => {
                   <div className="border-b border-gray-100 p-6 text-center sm:border-0 sm:border-r">
                     <p className="text-5xl leading-none font-extrabold text-blue-900">
                       2600
+                      {/* see stats.odt */}
                     </p>
                     <p className="mt-2 text-lg leading-6 font-medium text-gray-500">
                       Person hours
@@ -142,11 +120,13 @@ const ProjectsFlexpulse = () => {
                   <div className="border-t border-b border-gray-100 p-6 text-center sm:border-0 sm:border-l sm:border-r">
                     <p className="text-5xl leading-none font-extrabold text-blue-900">
                       1450
+                      {/* git shortlog -sn --no-merges */}
                     </p>
                     <p className="mt-2 text-lg leading-6 font-medium text-gray-500">
                       Commits
                     </p>
                   </div>
+                  {/* git log --shortstat --author="XXX" | grep -E "fil(e|es) changed" | awk '{files+=$1; inserted+=$4; deleted+=$6} END {print "files changed: ", files, "lines inserted: ", inserted, "lines deleted: ", deleted }' */}
                   <div className="border-t border-b border-gray-100 p-6 text-center sm:border-0 sm:border-l sm:border-r">
                     <p className="text-5xl leading-none font-extrabold text-green-900">
                       176K
@@ -179,7 +159,7 @@ const ProjectsFlexpulse = () => {
 const ProjectsUniversity = () => {
 
   const groupBadge = [
-    <p className="rounded-lg bg-pink-200 p-1 pl-2 pr-2 text-xs">
+    <p className="rounded-lg bg-pink-200 p-1 pl-2 pr-2 text-xs" key="group_badge">
       Group project
     </p>
   ]
@@ -368,28 +348,26 @@ const ProjectsUniversity = () => {
           <div className="mt-10">
             <div className="lg:grid lg:grid-cols-3 lg:gap-8 sm:grid sm:grid-cols-2">
               {projects_uni.reverse().map(({ name, description, grade, course, language, group }) => (
-                <div className="mt-5 ml-2.5 mr-2.5 rounded-lg bg-white">
-                  <div className="p-5">
-                    <div className="flex justify-between flex-initial">
-                      <p className="rounded-lg bg-blue-300 p-1 pl-2 pr-2">
-                        {language}
-                      </p>
-                      {group}
-                      <p className="rounded-lg bg-green-200 p-1 pl-2 pr-2" title="Grade">
-                        {grade}%
-                      </p>
-                    </div>
-                    <h5 className="mt-3 text-lg leading-6 font-medium text-gray-900">
-                      {name}
-                    </h5>
-                    <div className="mt-2 text-base leading-6 text-gray-500">
-                      <p>
-                        {description}
-                      </p>
-                      <p className="mt-2">
-                        <i>Course: {course}</i>
-                      </p>
-                    </div>
+                <div className="mt-5 ml-2.5 mr-2.5 p-5 rounded-lg bg-white" key={"project_uni_" + name}>
+                  <div className="flex justify-between flex-initial">
+                    <p className="rounded-lg bg-blue-300 p-1 pl-2 pr-2">
+                      {language}
+                    </p>
+                    {group}
+                    <p className="rounded-lg bg-green-200 p-1 pl-2 pr-2" title="Grade">
+                      {grade}%
+                    </p>
+                  </div>
+                  <h5 className="mt-3 text-lg leading-6 font-medium text-gray-900">
+                    {name}
+                  </h5>
+                  <div className="mt-2 text-base leading-6 text-gray-500">
+                    <p>
+                      {description}
+                    </p>
+                    <p className="mt-2">
+                      <i>Course: {course}</i>
+                    </p>
                   </div>
                 </div>
               ))}
